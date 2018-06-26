a5acf77cdab04bcfe5de9095fe62c0d8d8e15b3f
Alastair Smith <github@alastairsmith.me.uk>
2018-06-26 22:28:05 +0100

    Add repro code

--- Repro/Program.cs
+++ Repro/Program.cs
@@ -1,15 +1,19 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
+using Docker.DotNet;
+using Docker.DotNet.Models;
 
 namespace Repro
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
+            var client = new DockerClientConfiguration(new Uri("unix:///var/run/docker.sock")).CreateClient();
+            await client.Images.ListImagesAsync(new ImagesListParameters
+            {
+                All = true
+            });
         }
     }
 }
